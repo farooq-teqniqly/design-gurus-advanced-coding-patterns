cf9d3c5b0ad4c51a22cfb74bff7eaa64f72710b1
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-10-12 11:57:57 -0700

    Add counting functions and corresponding tests
    Implemented two functions, `counter_counter_func` and `dict_counter_func`, to count letter frequencies in a string using different methods. Added test cases to validate their correctness using `pytest`.

--- counting_patterns/examples.py
+++ counting_patterns/examples.py
@@ -1,14 +1,19 @@
-def count_letter_frequencies(input_str: str, counter_func) -> dict:
+from typing import Callable, Dict
+
+
+def count_letter_frequencies(
+    input_str: str, counter_func: Callable[[str], Dict[str, int]]
+) -> dict:
     return counter_func(input_str)
 
 
-def counter_counter_func(input_str: str) -> dict:
+def counter_counter_func(input_str: str) -> Dict[str, int]:
     from collections import Counter
 
     return Counter(input_str)
 
 
-def dict_counter_func(input_str: str) -> dict:
+def dict_counter_func(input_str: str) -> Dict[str, int]:
     histogram = {}
 
     for ch in input_str:
